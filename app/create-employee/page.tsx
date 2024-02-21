caf5da69c4f76e5ea7422d6ac057bd6ff0d2b857
hamllock <h4mlock@gmail.com>
2024-02-22 00:18:58 +0800

    Add table component for earnings

--- app/create-employee/page.tsx
+++ app/create-employee/page.tsx
@@ -5,6 +5,15 @@ import Link from 'next/link';
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 // Fields: First Name, Middle Name, Last Name, Street Address, Barangay, City, Province, Country, Zip Code, Status, Department, Position, Basic Pay, Income Tax
 // DropDown for Status: Active, Inactive
@@ -219,7 +228,27 @@ const CreateEmployeePage = () => {
           {/* TODO: Add a table for earnings that adds a row when Add Row button is clicked */}
           {/* Fields: Earnings, Amount */}
           <div>
-             <h2 className='font-bold text-lg'>Earnings</h2>     
+            <h2 className='font-bold text-lg'>Earnings</h2>    
+            <Table>
+              <TableCaption>A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Invoice</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Method</TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="font-medium">INV001</TableCell>
+                  <TableCell>Paid</TableCell>
+                  <TableCell>Credit Card</TableCell>
+                  <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+
           </div>
 
           {/* Deductions Table*/}
