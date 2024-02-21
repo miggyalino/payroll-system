34de5d7f6fa1ac81e7d96563097d3e1a32e078bb
Miguel Alino <miggyalino35@gmail.com>
2024-02-22 00:16:50 +0800

    fetchUtils boilerplate added. Not yet functional

--- utils/fetchUtils.ts
+++ utils/fetchUtils.ts
@@ -48,6 +48,40 @@ export const fetchEmployees = async () => {
 
   }
 
+  export const getEarnings = async () => {
+    const response = await fetch(`http://localhost:3000/api/payroll`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  
+    const data = await response.json();
+    return data;
+
+  }
+
+  export const getDeductions = async () => {
+    const response = await fetch(`http://localhost:3000/api/payroll`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  
+    const data = await response.json();
+    return data;
+
+  }
+
 
 
 
