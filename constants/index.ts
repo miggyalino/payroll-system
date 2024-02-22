653a862de3a3717a536faa12d35924daff7dc260
hamllock <h4mlock@gmail.com>
2024-02-22 14:02:49 +0800

    Added Lucide icons to sidebar

--- constants/index.ts
+++ constants/index.ts
@@ -1,24 +1,26 @@
 export const NAVBAR_ITEMS = [
     {
-    "id": "dashboard",
-    "href": "/",
-    "title": "Dashboard"
+        id: "dashboard",
+        href: "/",
+        title: "Dashboard",
+        icon: "dashboard-icon"
     },
     {
-    "id": "employees",
-    "href": "/employees-page",
-    "title": "Employees"
+        id: "employees",
+        href: "/employees-page",
+        title: "Employees",
+        icon: "employee-icon"
     },
     {
-    "id": "payroll",
-    "href": "/payroll-page",
-    "title": "Payroll"
+        id: "payroll",
+        href: "/payroll-page",
+        title: "Payroll",
+        icon: "payroll-icon"
     },
     {
-    "id": "company",
-    "href": "/company-page",
-    "title": "Company"
+        id: "company",
+        href: "/company-page",
+        title: "Company",
+        icon: "company-icon"
     }
 ]
-  
-  
